5010c2829c448db60455c9e53e65f9feb013492b
kiennguyen <nguyenanhkien2a@gmail.com>
2012-01-05 16:38:17 +0700

    Store shindig oauth consumer key in storage, create portlet to support editable consumer key

--- packaging/module/src/main/javascript/portal.packaging.module.js
+++ packaging/module/src/main/javascript/portal.packaging.module.js
@@ -211,6 +211,9 @@ function getModule(params)
    module.portlet.web =
    new Project("org.exoplatform.portal", "exo.portal.portlet.web", "exo-portlet", module.version);
 
+   module.portlet.shindigOauthStore =
+   new Project("org.exoplatform.portal", "exo.portal.portlet.shindig-oauth", "war", module.version);
+
    module.portlet.dashboard =
    new Project("org.exoplatform.portal", "exo.portal.portlet.dashboard", "exo-portlet", module.version).
       addDependency(new Project("org.exoplatform.portal", "exo.portal.webui.dashboard", "jar", module.version));
